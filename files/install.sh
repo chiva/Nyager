28b7d7d8c25c20e80507da7bd250db8a34698bd9
chiva <santireig@gmail.com>
2011-07-12 22:21:41 +0200

    updated install script

--- files/install.sh
+++ files/install.sh
@@ -8,7 +8,7 @@ echo "address=/#/192.168.1.1" > /etc/dnsmasq.conf
 mv jasager /etc/init.d/jasager
 chmod +x /etc/init.d/jasager
 
-mv bg.gif index.html mp3player.swf nyan-looped.mp3 snyan.gif stylesheet.css slkscr-webfont.eot slkscr-webfont.ttf /www/
+mv bg.gif index.html mp3player.swf nyan-looped.mp3 snyan.gif stylesheet.css slkscr-webfont.eot slkscr-webfont.ttf slkscr-webfont.woff slkscr-webfont.svg favicon.ico /www/
 
 echo "E404:/www/index.html" > /etc/nyhttpd.conf
 mv httpd /etc/init.d/httpd
